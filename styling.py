da4d6ebaea003b3d0eeb57579d0784b6959ab098
GenThrive <genthrive@ecorise.org>
2024-10-07 12:28:15 -0500

    updated information and format

--- styling.py
+++ styling.py
@@ -1,8 +1,8 @@
 # ----------------------------------------------------------------------------
 # PAGE SETTINGS
 # ----------------------------------------------------------------------------
-app_title = 'Ohio Dashboard - 2024'
-page_title = 'Ohio 2024'
+app_title = 'Ohio Environmental Education Directory'
+page_title = 'Ohio Environmental Education Directory'
 sub_title = 'Filter on'
 filter_category_1 = 'Organization Data'
 filter_category_2 = 'Program Data'
@@ -12,7 +12,7 @@ filter_category_2 = 'Program Data'
 # ----------------------------------------------------------------------------
 map_center_lat = 40.3579996
 map_center_lon = -83.9885444
-map_zoom = 5.072
+map_zoom = 5.472
 
 # Column in the dataset that maps to the featureidkey of the geojson.
 # This is what maps the data column to the geojson feature
